b8b45f1137d119daeb0b6f5c28f8f62f27254370
Peter Schmidt <schmidt.multimedia@gmail.com>
2024-01-31 14:23:52 +0100

    Add files via upload

---
+++ com/cc/java/HoneyBee.java
@@ -0,0 +1,16 @@
+package com.cc.java;
+
+public class HoneyBee {
+
+    public String doYourJob(){
+        return "I'm a HoneyBee!";
+    }
+
+    public String fly(){
+        return "I can fly!";
+    }
+
+
+
+
+}
