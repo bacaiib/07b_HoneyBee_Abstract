684d5159a007bcbfdc65421463d13612b13aabdc
bacaiib <roman-frommhold@gmx.net>
2024-02-01 11:58:14 +0100

    biiird

---
+++ com/cc/java/Bird.java
@@ -0,0 +1,17 @@
+package com.cc.java;
+
+public class Bird extends HoneyBee{
+
+    @Override
+    public String doYourJob() {
+        return "But i'm a biiiiird!";
+    }
+
+    @Override
+    public String fly() {
+        return "I can fly at 200mph!";
+    }
+
+    
+
+}
